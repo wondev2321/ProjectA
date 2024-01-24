47a441371a2ff541060aeead4dcd3b0853f115cb
wondev2321 <joowon2321@gmail.com>
2024-01-25 01:57:41 +0900

    init setting

---
+++ src/main/java/com/nndev/projecta/ProjectAApplication.java
@@ -0,0 +1,13 @@
+package com.nndev.projecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectAApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nndev/projecta/ProjectAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nndev.projecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
