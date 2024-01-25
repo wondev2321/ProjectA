4fac5c09a15aaaa24c71bbc7b28d62f612b729dd
wondev2321 <joowon2321@gmail.com>
2024-01-25 18:05:07 +0900

    feat : 개발 환경 세팅

--- src/main/java/com/nndev/projecta/ProjectAApplication.java
+++ src/main/java/com/nndev/projecta/ProjectAApplication.java
@@ -5,9 +5,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectAApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ProjectAApplication.class, args);
-    }
-
+    public static void main(String[] args){ SpringApplication.run(ProjectAApplication.class, args); }
 }
